65b819b322cca24a0b8d5a386b26f3c32fc6ec42
Parameshwar Venishetty <parameshwar.venishetty@ggktech.local>
2018-05-09 19:18:12 +0530

    Updated code

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 
 import { RouterModule, Routes } from '@angular/router';
 import {LoginModule} from './modules/login/login.module';
+import {DashboardModule} from '@dashboard/dashboard.module';
 
 export function loadLoginModule() { return LoginModule; }
+export function loadDashboardModule() { return DashboardModule; }
+
 const routes: Routes = [
-  { path: 'login', loadChildren: loadLoginModule }  
+  { path: 'login', loadChildren: loadLoginModule },
+  { path: 'dashboard', loadChildren: loadDashboardModule }
 ];
 
 
